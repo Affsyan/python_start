45060ce4bed0d527b876acd4236ab1d577aaf5e9
Affsyan <Affsyan@gmail.com>
2020-10-24 20:12:45 +0300

    les1

--- homeworks/les1/task4.py
+++ homeworks/les1/task4.py
@@ -1,18 +1,16 @@
 # 4. Пользователь вводит целое положительное число. Найдите самую большую цифру в числе.
 # Для решения используйте цикл while и арифметические операции.
-# user_number = int(input("Введите число: "))
-#
-# print(user_number%10)
-# print(user_number//10)
-# print("")
-# while user_number > 10:
-#     number = user_number % 10
-#     if number < user_number:
-#         user_number = user_number // 10
-#         max_number = user_number
-#     else:
-#         max_number = number
-# print(max_number)
+
+user_number = int(input("Введите число: "))
+
+max_number = user_number%10
+user_number = user_number//10
+
+while user_number > 0:
+    if user_number%10 > max_number:
+        max_number = user_number%10
+    user_number = user_number//10
+print(f"Максимальная цифра в числе {max_number}")
 
 
 
