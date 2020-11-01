55dfea21ef22f7947019f2b3499129fa03f8add7
Affsyan <Affsyan@gmail.com>
2020-11-01 16:23:34 +0300

    les3

--- homeworks/task3.py
+++ homeworks/task3.py
@@ -2,27 +2,31 @@
 # и возвращает сумму наибольших двух аргументов.
 
 
-def check(a):
+def check(user_len):
     while True:
-        if len(a) != 3:
+        try:
+            for el in user_len:
+                int(el)
+        except ValueError:
             print('Ошибка ввода')
-            a = input('Введите 3 числа через пробел: ').split(' ')
-            if type(a) != tuple:
-                print('Ошибка ввода')
-                a = input('Введите 3 числа через пробел: ').split(' ')
+            user_len = input('Введите 3 числа через пробел: ').split(' ')
             continue
-        else:
-            try:
-                a = float(a[0]) + float(a[1]) + float(a[2])
-            except ValueError:
-                print('Ошибка ввода')
-                a = input('Введите 3 числа через пробел: ').split(' ')
-                continue
+        if len(user_len) != 3:
+            print('Ошибка ввода')
+            user_len = input('Введите 3 числа через пробел: ').split(' ')
+            continue
         break
-    print(a)
-    return a
+    print(user_len)
+    return user_len
+
+
+def sum_number(*args):
+    args = sorted(args)
+    print(args)
+    print(f'Самые большие числа {args[-1]} и {args[-2]}')
+    print(f'Сумма чисел равна: {int(args[-2]) + int(args[-1])}')
+
 
 user_number = input('Введите 3 числа через пробел: ').split(' ')
-number = check(user_number)
-
-
+check(user_number)
+sum_number(*user_number)
