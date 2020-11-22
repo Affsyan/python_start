5e68ceee7a9764af1261fed6ad9d451ee7853690
Affsyan <Affsyan@gmail.com>
2020-11-22 19:43:03 +0300

    les7

--- homeworks/task1.py
+++ homeworks/task1.py
@@ -17,9 +17,35 @@ class Data:
 
     @staticmethod
     def valid(number):
-        if  31 < number[0] < 1
+        m30 = (4, 6, 9, 11)  # месяцы с 30 днями
+        m31 = (1, 3, 5, 7, 8, 10, 12)  # месяцы с 31 днём
+        if number[2] % 4 != 0 or (number[2] % 100 == 0 and number[2] % 400 != 0):
+            leap_year = False
+        else:
+            leap_year = True
+        if number[1] > 12:
+            return print('Неправильный ввод')
+        else:
+            if number[1] in m30:
+                if number[0] >= 31:
+                    return print('Неправильный ввод')
+                else:
+                    return print('Данные корректны')
+            elif number[1] in m31:
+                if number[0] >= 32:
+                    return print('Неправильный ввод')
+                else:
+                    return print('Данные корректны')
+            else:
+                if number[0] >= 29 and not leap_year:
+                    return print('Неправильный ввод')
+                elif number[0] >= 30 and leap_year:
+                    return print('Неправильный ввод')
+                else:
+                    return print('Данные корректны')
 
 
-
-test = Data("15-11-2020")
+test = Data("29-2-2019")
+Data.number()
+test2 = Data("29-2-2020")
 Data.number()
